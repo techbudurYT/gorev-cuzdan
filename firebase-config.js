93b1e7e90a5d185bfabfbfce46cf1adcb83001ec
techbudurYT <98665965+techbudurYT@users.noreply.github.com>
2025-09-13 21:50:14 +0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { getFirestore, collection, doc, setDoc, getDoc, onSnapshot, query, where, orderBy, getDocs, runTransaction, addDoc, serverTimestamp, updateDoc, limit } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBN85bxThpJYifWAvsS0uqPD0C9D55uPpM",
-  authDomain: "gorev-cuzdan.firebaseapp.com",
-  projectId: "gorev-cuzdan",
-  storageBucket: "gorev-cuzdan.firebasestorage.app",
-  messagingSenderId: "139914511950",
-  appId: "1:139914511950:web:0d7c9352e410223742e51f",
-  measurementId: "G-FV2YZCT93N"
+    apiKey: "AIzaSyBN85bxThpJYifWAvsS0uqPD0C9D55uPpM",
+    authDomain: "gorev-cuzdan.firebaseapp.com",
+    projectId: "gorev-cuzdan",
+    storageBucket: "gorev-cuzdan.appspot.com",
+    messagingSenderId: "139914511950",
+    appId: "1:139914511950:web:0d7c9352e410223742e51f"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
